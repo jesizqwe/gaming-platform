d0eb2062498ea124827830845cd94ddf415d8cc7
j <san4ez131018@gmail.com>
2026-02-12 18:38:05 +0300

    fix for server

--- client/src/hooks/useSocket.ts
+++ client/src/hooks/useSocket.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { io, Socket } from 'socket.io-client';
 
-const SOCKET_URL = 'http://localhost:3000';
+const SOCKET_URL = 'https://gaming-platform-xdkr.onrender.com';
 
 export function useSocket() {
   const [socket, setSocket] = useState<Socket | null>(null);
